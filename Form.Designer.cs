d43eb216534c1853d355dbc5a43a0ed459033b39
Mettage <57479892+Mettage@users.noreply.github.com>
2021-04-21 14:30:27 +0200

    Initial commit

---
+++ Form.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Robocopy {
+partial class window
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose( bool disposing )
+    {
+        if ( disposing && ( components != null ) )
+        {
+            components.Dispose();
+        }
+        base.Dispose( disposing );
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager( typeof( window ) );
+        this.sourceLabel = new System.Windows.Forms.Label();
+        this.destinationLabel = new System.Windows.Forms.Label();
+        this.sourceTextBox = new System.Windows.Forms.TextBox();
+        this.sourceBrowseButton = new System.Windows.Forms.Button();
+        this.destinationBrowseButton = new System.Windows.Forms.Button();
+        this.destinationTextBox = new System.Windows.Forms.TextBox();
+        this.doitButton = new System.Windows.Forms.Button();
+        this.SuspendLayout();
+        //
+        // sourceLabel
+        //
+        this.sourceLabel.AutoSize = true;
+        this.sourceLabel.Location = new System.Drawing.Point( 12, 9 );
+        this.sourceLabel.Name = "sourceLabel";
+        this.sourceLabel.Size = new System.Drawing.Size( 44, 13 );
+        this.sourceLabel.TabIndex = 0;
+        this.sourceLabel.Text = "Source:";
+        //
+        // destinationLabel
+        //
+        this.destinationLabel.AutoSize = true;
+        this.destinationLabel.Location = new System.Drawing.Point( 12, 48 );
+        this.destinationLabel.Name = "destinationLabel";
+        this.destinationLabel.Size = new System.Drawing.Size( 63, 13 );
+        this.destinationLabel.TabIndex = 1;
+        this.destinationLabel.Text = "Destination:";
+        //
+        // sourceTextBox
+        //
+        this.sourceTextBox.Location = new System.Drawing.Point( 12, 25 );
+        this.sourceTextBox.Name = "sourceTextBox";
+        this.sourceTextBox.Size = new System.Drawing.Size( 291, 20 );
+        this.sourceTextBox.TabIndex = 2;
+        this.sourceTextBox.TextChanged += new System.EventHandler( this.sourceTextBox_TextChanged );
+        //
+        // sourceBrowseButton
+        //
+        this.sourceBrowseButton.Location = new System.Drawing.Point( 309, 23 );
+        this.sourceBrowseButton.Name = "sourceBrowseButton";
+        this.sourceBrowseButton.Size = new System.Drawing.Size( 75, 23 );
+        this.sourceBrowseButton.TabIndex = 3;
+        this.sourceBrowseButton.Text = "Browse";
+        this.sourceBrowseButton.UseVisualStyleBackColor = true;
+        this.sourceBrowseButton.Click += new System.EventHandler( this.sourceBrowseButton_Click );
+        //
+        // destinationBrowseButton
+        //
+        this.destinationBrowseButton.Location = new System.Drawing.Point( 309, 62 );
+        this.destinationBrowseButton.Name = "destinationBrowseButton";
+        this.destinationBrowseButton.Size = new System.Drawing.Size( 75, 23 );
+        this.destinationBrowseButton.TabIndex = 4;
+        this.destinationBrowseButton.Text = "Browse";
+        this.destinationBrowseButton.UseVisualStyleBackColor = true;
+        this.destinationBrowseButton.Click += new System.EventHandler( this.destinationBrowseButton_Click );
+        //
+        // destinationTextBox
+        //
+        this.destinationTextBox.Location = new System.Drawing.Point( 12, 64 );
+        this.destinationTextBox.Name = "destinationTextBox";
+        this.destinationTextBox.Size = new System.Drawing.Size( 291, 20 );
+        this.destinationTextBox.TabIndex = 5;
+        this.destinationTextBox.TextChanged += new System.EventHandler( this.destinationTextBox_TextChanged );
+        //
+        // doitButton
+        //
+        this.doitButton.Location = new System.Drawing.Point( 390, 23 );
+        this.doitButton.Name = "doitButton";
+        this.doitButton.Size = new System.Drawing.Size( 70, 62 );
+        this.doitButton.TabIndex = 6;
+        this.doitButton.Text = "DO IT!";
+        this.doitButton.UseVisualStyleBackColor = true;
+        this.doitButton.Click += new System.EventHandler( this.doitButton_Click );
+        //
+        // window
+        //
+        this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size( 479, 98 );
+        this.Controls.Add( this.doitButton );
+        this.Controls.Add( this.destinationTextBox );
+        this.Controls.Add( this.destinationBrowseButton );
+        this.Controls.Add( this.sourceBrowseButton );
+        this.Controls.Add( this.sourceTextBox );
+        this.Controls.Add( this.destinationLabel );
+        this.Controls.Add( this.sourceLabel );
+        this.Icon = ( ( System.Drawing.Icon )( resources.GetObject( "$this.Icon" ) ) );
+        this.Name = "window";
+        this.Text = "ROBOCOP";
+        this.FormClosing += new System.Windows.Forms.FormClosingEventHandler( this.window_FormClosing );
+        this.ResumeLayout( false );
+        this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label sourceLabel;
+    private System.Windows.Forms.Label destinationLabel;
+    private System.Windows.Forms.TextBox sourceTextBox;
+    private System.Windows.Forms.Button sourceBrowseButton;
+    private System.Windows.Forms.Button destinationBrowseButton;
+    private System.Windows.Forms.TextBox destinationTextBox;
+    private System.Windows.Forms.Button doitButton;
+}
+}
+
---
+++ Form.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace Robocopy {
+public partial class window : Form
+{
+    static string currentDir = Environment.CurrentDirectory;
+    static string settings = currentDir + "\\settings.ini";
+    static string robocopB = currentDir + "\\robocop.bat";
+    static string sourceD = "";
+    static string destinationD = "";
+    public window()
+    {
+        Remove();
+        Inicialize();
+        InitializeComponent();
+        doitButton.Enabled = false;
+        sourceTextBox.Text = sourceD;
+        destinationTextBox.Text = destinationD;
+    }
+
+    void Remove()
+    {
+        if ( File.Exists( robocopB ) )
+        {
+            File.Delete( robocopB );
+        }
+    }
+
+    void Inicialize()
+    {
+        if ( File.Exists( settings ) )
+        {
+            StreamReader sett = new StreamReader( settings );
+            int lineNumber = 0;
+            string line = sett.ReadLine();
+            while ( line != null )
+            {
+                lineNumber++;
+                line = sett.ReadLine();
+                if ( lineNumber == 1 ) { sourceD = line; }
+                if ( lineNumber == 2 ) { destinationD = line; }
+            }
+            sett.Close();
+        }
+        else
+        {
+            using ( FileStream fileS = new FileStream( settings, FileMode.Create ) ) { }
+            Set();
+        }
+    }
+
+    void Set()
+    {
+        StreamWriter settings = new StreamWriter( "settings.ini" );
+        settings.WriteLine( "*** ROBOCOY settings ***" );
+        settings.WriteLine( sourceD );
+        settings.WriteLine( destinationD );
+        settings.Close();
+    }
+
+    void ButEna()
+    {
+        if ( Directory.Exists( sourceD ) && Directory.Exists( destinationD ) )
+        {
+            doitButton.Enabled = true;
+        }
+        else
+        {
+            doitButton.Enabled = false;
+        }
+    }
+
+    private async void Doit()
+    {
+        Task<int> doitc = null;
+        doitc = Roboco();
+        doitButton.Enabled = false;
+        await doitc;
+        doitButton.Enabled = true;
+    }
+
+    private Task<int> Roboco()
+    {
+        using ( StreamWriter command = new StreamWriter( robocopB ) )
+        {
+            command.WriteLine( $"@echo off" );
+            command.WriteLine( $"robocopy " + sourceD + " " + destinationD );
+            command.Close();
+        }
+        return RunProcessAsync( robocopB );
+    }
+
+    private Task<int> RunProcessAsync( string fileName )
+    {
+        var tcs = new TaskCompletionSource<int>();
+        var process = new Process
+        {
+            StartInfo = { FileName = fileName },
+            EnableRaisingEvents = true
+        };
+        process.Exited += ( sender, args ) =>
+        {
+            tcs.SetResult( process.ExitCode );
+            process.Dispose();
+        };
+
+        process.Start();
+        return tcs.Task;
+    }
+
+    private void sourceTextBox_TextChanged( object sender, EventArgs e )
+    {
+        sourceD = sourceTextBox.Text;
+        ButEna();
+        if ( Directory.Exists( sourceD ) ) { Set(); ButEna(); }
+    }
+
+    private void sourceBrowseButton_Click( object sender, EventArgs e )
+    {
+        FolderBrowserDialog sourceF = new FolderBrowserDialog();
+        sourceF.SelectedPath = sourceD;
+        sourceF.Description = "Choose Source folder";
+        if ( sourceF.ShowDialog() == DialogResult.OK )
+        {
+            sourceTextBox.Text = sourceF.SelectedPath;
+            sourceD = sourceF.SelectedPath;
+            Set();
+        }
+    }
+
+    private void destinationTextBox_TextChanged( object sender, EventArgs e )
+    {
+        destinationD = destinationTextBox.Text;
+        ButEna();
+        if ( Directory.Exists( destinationD ) ) { Set(); }
+    }
+
+    private void destinationBrowseButton_Click( object sender, EventArgs e )
+    {
+        FolderBrowserDialog destinationF = new FolderBrowserDialog();
+        destinationF.SelectedPath = destinationD;
+        destinationF.Description = "Choose Destination folder";
+        if ( destinationF.ShowDialog() == DialogResult.OK )
+        {
+            destinationTextBox.Text = destinationF.SelectedPath;
+            destinationD = destinationF.SelectedPath;
+            Set();
+        }
+    }
+
+    private void doitButton_Click( object sender, EventArgs e )
+    {
+        Doit();
+    }
+
+    private void window_FormClosing( object sender, FormClosingEventArgs e )
+    {
+        Remove();
+    }
+}
+}
